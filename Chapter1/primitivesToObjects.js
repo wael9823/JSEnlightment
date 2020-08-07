98732b2033e51e839f37f1a1def2864220a6c033
Mohammed Wael <mohd.wael9823@gmail.com>
2020-08-07 12:31:45 +0530

    Added comments for primitive values as objects

--- Chapter1/primitivesToObjects.js
+++ Chapter1/primitivesToObjects.js
@@ -3,22 +3,22 @@ var primNumber2 = primNumber1;
 
 
 function printPrimType(){
-    console.log(typeof primNumber1);
+    console.log(typeof primNumber1);  //logs number
     return(typeof primNumber1);
 }
 
 function printPrimString(){
-    console.log(primNumber1.toString());
+    console.log(primNumber1.toString()); //prints number as string and works as a complex object
     return(primNumber1.toString());
 }
 
 function printStringType(){
-    console.log(typeof primNumber1.toString());
+    console.log(typeof primNumber1.toString()); //logs string
     return(typeof primNumber1.toString());
 }
 
 function printPrimBack(){
-    console.log(typeof primNumber1);
+    console.log(typeof primNumber1); //logs as number again
     return(typeof primNumber1);
 }
 

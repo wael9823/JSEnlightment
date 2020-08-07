17e7987dc6cd1aecefa43c6c6378d058849afcf7
Mohammed Wael <mohd.wael9823@gmail.com>
2020-08-07 07:21:43 +0530

    Added test for objectCreation Program

---
+++ Chapter1/objectCreation.test.js
@@ -0,0 +1,22 @@
+
+const waell = require('./objectCreation');
+const age = waell.age;
+const mgender = waell.gender;
+const twork = waell.working;
+
+
+test ('Return Gender properly', () => {
+    expect(mgender()).toEqual('male');
+});
+
+test ('Return true for working', () => {
+    expect(twork()).toBeTruthy;
+});
+
+test ('Crosschecking Gender Value', () => {
+    expect(mgender()).not.toEqual('Female');
+});
+
+test ('Age value is right or not', () => {
+    expect(age()).toEqual(21);
+})
